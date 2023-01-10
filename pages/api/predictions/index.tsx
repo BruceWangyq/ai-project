899f411eea1140a86a918704146296052bc2e9ec
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-01-09 23:06:39 -0500

    feat: add api and fetch feature

---
+++ pages/api/predictions/index.tsx
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // Pinned to a specific version of Stable Diffusion
+      // See https://replicate.com/stability-ai/stable-diffussion/versions
+      version:
+        "6359a0cab3ca6e4d3320c33d79096161208e9024d174b2311e5a21b6c7e1131c",
+
+      // This is the text prompt that will be submitted by a form on the frontend
+      input: { prompt: req.body.prompt },
+    }),
+  });
+
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
