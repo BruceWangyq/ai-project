bc2bebcc88a1dc68dc02189085b12de496deac7a
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-01-22 23:36:34 -0500

    feat: add resizeable panel

--- components/ResizeablePanel.tsx
+++ components/ResizeablePanel.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
+import useMeasure from "react-use-measure";
 
-type Props = {}
+type Props = {};
 
-function ResizeablePanel({children}: children: React.ReactNode) {
-    let [ref, { height}] = useMeasure()
+function ResizeablePanel({ children }: { children: React.ReactNode }) {
+  let [ref, { height }] = useMeasure();
   return (
-    <div>ResizeablePanel</div>
-  )
+    <motion.div
+      animate={height ? { height } : {}}
+      style={height ? { height } : {}}
+      className="relative w-full overflow-hidden"
+      transition={{ type: "tween", duration: 0.5 }}
+    >
+      <div ref={ref} className={height ? "absolute inset-x-0" : "relative"}>
+        {children}
+      </div>
+    </motion.div>
+  );
 }
 
-export default ResizeablePanel
+export default ResizeablePanel;
