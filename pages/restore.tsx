8ba04dc5801ffbd4ab51dc50b68e30369fd723f6
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-01-23 23:40:38 -0500

    feat: add restore

--- pages/restore.tsx
+++ pages/restore.tsx
@@ -1,9 +1,12 @@
 import { setDefaultResultOrder } from "dns";
+import { AnimatePresence, motion } from "framer-motion";
 import { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { useState } from "react";
 import { UploadDropzone } from "react-uploader";
 import { Uploader } from "uploader";
+import ResizeablePanel from "../components/ResizeablePanel";
 
 type Props = {};
 
@@ -22,23 +25,26 @@ const options = {
 
 const Home: NextPage<Props> = () => {
   const [originalPhoto, setOriginalPhoto] = useState<string | null>(null);
+  const [restoredImage, setRestoredImage] = useState<string | null>(null);
   const [PhotoName, setPhotoName] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [sideBySide, setSideBySide] = useState<boolean>(false);
+  const [restoredLoaded, setRestoredLoaded] = useState<boolean>(false);
 
   const UploadDropZone = () => {
     <UploadDropzone
       uploader={uploader}
       options={options}
-      onUpload={(file) => {
+      onUpdate={(file) => {
         if (file.length !== 0) {
-          setPhotoName(file[0].originalFile.originalName);
+          setPhotoName(file[0].originalFile.originalFileName);
           setOriginalPhoto(file[0].fileUrl.replace("raw", "thumbnail"));
           generatePhoto(file[0].fileUrl.replace("raw", "thumbnail"));
         }
       }}
-      width={300}
-      height={300}
+      width="670px"
+      height="250px"
     />;
   };
 
@@ -67,10 +73,54 @@ const Home: NextPage<Props> = () => {
         <title>Restore</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="mx-auto max-w-4xl font-bold text-4xl tracking-normal text-slate-900 sm:text-6xl mb-5 ">
-        Restore any face photo
-      </h1>
-      <ResizeablePanel></ResizeablePanel>
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4 sm:mb-0 mb-8">
+        <h1 className="mx-auto max-w-4xl font-bold text-4xl tracking-normal text-slate-900 sm:text-6xl mb-5 ">
+          Restore any face photo
+        </h1>
+        <ResizeablePanel>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div className="flex justify-between items-center w-full flex-col mt-4">
+              {!originalPhoto && <UploadDropZone />}
+              {originalPhoto && !restoredImage && (
+                <Image
+                  alt="original photo"
+                  src={originalPhoto}
+                  className="rounded-2xl"
+                  width={475}
+                  height={475}
+                />
+              )}
+              {restoredImage && originalPhoto && !sideBySide && (
+                <div className="flex sm:space-x-4 sm:flex-row flex-col">
+                  <div>
+                    <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                    <Image
+                      alt="original photo"
+                      src={originalPhoto}
+                      className="rounded-2xl relative"
+                      width={475}
+                      height={475}
+                    />
+                  </div>
+                  <div className="sm:mt-0 mt-8">
+                    <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                    <a href={restoredImage} target="_blank" rel="noreferrer">
+                      <Image
+                        alt="restored photo"
+                        src={restoredImage}
+                        className="rounded-2xl relative sm:mt-0 mt-2 cursor-zoom-in"
+                        width={475}
+                        height={475}
+                        onLoadingComplete={() => setRestoredLoaded(true)}
+                      />
+                    </a>
+                  </div>
+                </div>
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </ResizeablePanel>
+      </main>
     </div>
   );
 };
