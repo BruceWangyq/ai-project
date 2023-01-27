bdf9d49000b76815201b1418e4f86e193dcfb000
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-01-27 00:29:32 -0500

    feat: change api key name

--- pages/api/predictions/[id].tsx
+++ pages/api/predictions/[id].tsx
@@ -8,7 +8,7 @@ export default async function handler(
     "https://api.replicate.com/v1/predictions/" + req.query.id,
     {
       headers: {
-        Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`,
+        Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
         "Content-Type": "application/json",
       },
     }
