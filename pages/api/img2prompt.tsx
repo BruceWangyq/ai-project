fc2460f09d704a394b3e0f85e0e0d1ef08823def
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-01-27 22:09:50 -0500

    feat: add rate limit

--- pages/api/img2prompt.tsx
+++ pages/api/img2prompt.tsx
@@ -1,4 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import requestIp from "request-ip";
+import redis from "../../utils/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 
 type Data = string;
 interface ExtendedNextApiRequest extends NextApiRequest {
@@ -7,10 +10,33 @@ interface ExtendedNextApiRequest extends NextApiRequest {
   };
 }
 
+// Create a new ratelimiter, that allows 3 requests per 60 seconds
+const ratelimit = redis
+  ? new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.fixedWindow(3, "60 s"),
+    })
+  : undefined;
+
 export default async function handler(
   req: ExtendedNextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (ratelimit) {
+    const identifier = requestIp.getClientIp(req);
+    const result = await ratelimit.limit(identifier!);
+    res.setHeader("X-RateLimit-Limit", result.limit);
+    res.setHeader("X-RateLimit-Remaining", result.remaining);
+
+    if (!result.success) {
+      res
+        .status(429)
+        .json(
+          "Too many uploads in 1 minute. Please try again in a few minutes."
+        );
+      return;
+    }
+  }
   const imageUrl = req.body.imageUrl;
   let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
