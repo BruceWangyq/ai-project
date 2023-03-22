114ac6c68d2a31337f2f478743d132d487d8cca7
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-03-22 11:24:16 -0400

    fix: auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,9 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -19,6 +22,39 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
 };
 
 export default NextAuth(authOptions);
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,6 @@
-import Stripe from "stripe";
+import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
-});
+export const stripe = new Stripe(process.env.STRIPE_API_KEY || "", {
+  apiVersion: "2022-11-15",
+  typescript: true,
+})
